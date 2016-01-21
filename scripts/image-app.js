25369c38f6ee95f99f6e324259b750528e7ec556
shawnajean <shawnajean@users.noreply.github.com>
2016-01-20 18:36:29 -0600

    Adding worker thread

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -5,6 +5,7 @@
   imageLoader.addEventListener('change', handleImage, false);
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
+  var worker = new Worker('worker.js');
 
   function handleImage(e){
     var reader = new FileReader();
@@ -42,8 +43,9 @@
 
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
+    imageData = worker.postMessage({imageData : imageData, type : type});
 
-    length = imageData.data.length / 4;
+    /*length = imageData.data.length / 4;
     for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
       r = imageData.data[i * 4 + 0];
       g = imageData.data[i * 4 + 1];
@@ -54,7 +56,7 @@
       imageData.data[i * 4 + 1] = pixel[1];
       imageData.data[i * 4 + 2] = pixel[2];
       imageData.data[i * 4 + 3] = pixel[3];
-    }
+  }*/
     toggleButtonsAbledness();
     return ctx.putImageData(imageData, 0, 0);
   };
